32ab14fb36bcc1a906b4adcf94d26d62f2edf211
vishalratanshakya <vishalratanshakya@gmail.com>
2026-09-18 12:50:51 +0530

    Rename nexora folder to homecraftservices

--- nexora/frontend/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ['uneasily-wildly-proactive.ngrok-free.dev']
-};
-
-export default nextConfig;
